d727f2f37a1513810ad92e6ed7b352b3af23cd67
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-06 22:27:42 +0700

    progres to try first challenge

---
+++ src/challenge/fundamental/cs_1.go
@@ -0,0 +1,17 @@
+/*
+* FGA - Hactiv8
+* Challenge 1
+* Muhammad Ferdian Iqbal
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	i := 21
+	j := true
+	fmt.Println(i)
+	fmt.Println("%")
+	fmt.Println(j)
+}
